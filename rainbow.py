905e9c1c26ce8df95090732bc2140b570df68b97
Killian Davitt <davittk@tcd.ie>
2016-04-13 17:21:37 +0100

    Interesting changes

--- rainbow.py
+++ rainbow.py
@@ -1,17 +1,21 @@
 import hashlib
-from itertools import permutations
+from itertools import combinations_with_replacement
+import itertools
 
 def main():
     
-    key_space = ['a','b','c','d','e']
+    key_space = ['a','i','l','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','H','A']
 
-    for key in permutations(key_space):
-        print(key)
+    for key in map(''.join, itertools.product('AHialn ', repeat=7)):
         md5_obj = hashlib.md5()
         md5_obj.update(''.join(key).encode('utf-8'))
         hashed_key = md5_obj.hexdigest()
-        print(hashed_key)
-    
+        if key == "Hi Alan":
+            print(hashed_key)
+
+        if hashed_key == "4471c23e354c63c4d0dc3d69c1162317":
+            print(key)
+
 
 
 if __name__ == "__main__":
